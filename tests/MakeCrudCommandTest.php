04c20bd4fa394ca9b33ad263f1ac8e8dd2f3225c
miladev-ent <milwad.dev@gmail.com>
2022-09-03 22:33:24 +0430

    complete migration file exists test

--- tests/MakeCrudCommandTest.php
+++ tests/MakeCrudCommandTest.php
@@ -30,7 +30,8 @@ class MakeCrudCommandTest extends BaseTest
     {
         $this->artisan("crud:make $this->name");
 
-        $this->checkModelExitst();
+        $this->checkModelExists();
+        $this->checkMigrationExists();
     }
 
     /**
@@ -38,8 +39,47 @@ class MakeCrudCommandTest extends BaseTest
      *
      * @return void
      */
-    private function checkModelExitst()
+    private function checkModelExists()
     {
         $this->assertEquals(1, file_exists(base_path("App\Models\\$this->name.php")));
+    }
+
+    /**
+     * Check migration is exists.
+     *
+     * @return void
+     */
+    private function checkMigrationExists()
+    {
+        $this->name = strtolower($this->name);
+        
+        if (!str_ends_with($this->name, 'y')) {
+            $file = $this->migrationExists("create_{$this->name}s_table");
+        } else {
+            $file = $this->migrationExists("create_{$this->name}ies_table");
+        }
+
+        $this->assertEquals(1, $file);
+    }
+
+    /**
+     * Check migration file is exists.
+     *
+     * @param  $mgr
+     * @return bool
+     */
+    private function migrationExists($mgr)
+    {
+        $path = database_path('migrations/');
+        $files = scandir($path);
+
+        foreach ($files as &$value) {
+            $pos = strpos($value, $mgr);
+            if ($pos !== false) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
