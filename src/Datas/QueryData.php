ccea796b72f9e54f72290f8da508cc59cde61d47
miladev-ent <milwad.dev@gmail.com>
2023-01-25 20:38:08 +0330

    add QueryData

---
+++ src/Datas/QueryData.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Milwad\LaravelCrod\Datas;
+
+/*
+ * This file is for add data in crud files.
+ *
+ * ======================== WARNING ======================== => DO NOT MAKE ANY CHANGES TO THIS FILE
+ */
+class QueryData
+{
+
+}
