17488dd22a777f66b452afa3940d4a83f0fb68b9
miladev-ent <milwad.dev@gmail.com>
2023-01-25 21:02:11 +0330

    add phpdoc into QueryData

--- src/Datas/QueryData.php
+++ src/Datas/QueryData.php
@@ -9,11 +9,25 @@ namespace Milwad\LaravelCrod\Datas;
  */
 class QueryData
 {
+    /**
+     * Get model data.
+     *
+     * @param mixed $items
+     * @return string
+     */
     public static function getModelData(mixed $items)
     {
         return PHP_EOL . '    protected $fillable = [' . $items . '];' . PHP_EOL . '}';
     }
 
+    /**
+     * Get service data.
+     *
+     * @param string $model
+     * @param string $request
+     * @param string $id
+     * @return string
+     */
     public static function getServiceData(string $model, string $request, string $id)
     {
         return "    public function store($id)
@@ -27,6 +41,12 @@ class QueryData
     }";
     }
 
+    /**
+     * Get use for service.
+     *
+     * @param string $model
+     * @return string
+     */
     public static function getUseServiceData(string $model)
     {
         return "
@@ -34,6 +54,13 @@ use App\Models\{$model};
 ";
     }
 
+    /**
+     * Get repo data.
+     *
+     * @param string $model
+     * @param string $id
+     * @return string
+     */
     public static function getRepoData(string $model, string $id)
     {
         return "    public function index()
@@ -52,6 +79,12 @@ use App\Models\{$model};
     }";
     }
 
+    /**
+     * Get repo for data.
+     *
+     * @param string $model
+     * @return string
+     */
     public static function getUseRepoData(string $model)
     {
         return "
@@ -59,6 +92,14 @@ use App\Models\{$model};
 ";
     }
 
+    /**
+     * Get controller-id data.
+     *
+     * @param string $comment
+     * @param string $request
+     * @param string $id
+     * @return string
+     */
     public static function getControllerIdData(string $comment, string $request, string $id)
     {
         return "    public function index()
@@ -92,6 +133,15 @@ use App\Models\{$model};
     }";
     }
 
+    /**
+     * Get controller route model binding.
+     *
+     * @param string $comment
+     * @param string $request
+     * @param string $name
+     * @param string $lowerName
+     * @return string
+     */
     public static function getControllerRouteModelBinding(string $comment, string $request, string $name, string $lowerName)
     {
         return "    public function index()
@@ -124,5 +174,4 @@ use App\Models\{$model};
         $comment
     }";
     }
-
 }
