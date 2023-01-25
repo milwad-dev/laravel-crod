6e1d88e465c475ab0b8a9183cbc6aaf19c5ce685
miladev-ent <milwad.dev@gmail.com>
2023-01-25 22:05:37 +0330

    fix str_ends_with

--- src/Commands/Modules/MakeCrudModuleCommand.php
+++ src/Commands/Modules/MakeCrudModuleCommand.php
@@ -215,7 +215,7 @@ class MakeCrudModuleCommand extends Command
      */
     private function getRealNameForMigration(string $name): string
     {
-        if (str_ends_with($name, 'y')) {
+        if (\str_ends_with($name, 'y')) {
             $name = substr_replace($name ,"", -1);
             return "create{$name}ies_table";
         }
