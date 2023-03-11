7dc52087027adc9e4e29fcc07f84eedfa0a74456
miladev-ent <milwad.dev@gmail.com>
2023-03-11 20:59:57 +0330

    add phpdoc for config file

--- config/laravel-crod.php
+++ config/laravel-crod.php
@@ -12,33 +12,43 @@ return [
     /*
      * Modules config.
      *
-     * You can make custom modules with special folders ...
+     * You can make custom modules with special folders ... .
      */
     'modules' => [
-        'module_namespace' => 'Modules',
-        'model_path' => 'Entities',
-        'migration_path' => 'Database\Migrations',
-        'controller_path' => 'Http\Controllers',
-        'request_path' => 'Http\Requests',
-        'view_path' => 'Resources\Views',
-        'service_path' => 'Services',
-        'repository_path' => 'Repositories',
-        'feature_test_path' => 'Tests\Feature',
-        'unit_test_path' => 'Tests\Unit',
-        'provider_path' => 'Providers',
+        'module_namespace' => 'Modules', // This value is for the name of the folder that the modules are in.
+        'model_path' => 'Entities', // This value is for the name of the folder that contains the module models.
+        'migration_path' => 'Database\Migrations', // This value is for the name of the folder that contains the module migrations.
+        'controller_path' => 'Http\Controllers', // This value is for the name of the folder that contains the module controllers.
+        'request_path' => 'Http\Requests', // This value is for the name of the folder that contains the module requests-form.
+        'view_path' => 'Resources\Views', // This value is for the name of the folder that contains the module views.
+        'service_path' => 'Services', // This value is for the name of the folder that contains the module services.
+        'repository_path' => 'Repositories', // This value is for the name of the folder that contains the module Repositories.
+        'feature_test_path' => 'Tests\Feature', // This value is for the name of the folder that contains the module feature-tests.
+        'unit_test_path' => 'Tests\Unit', // This value is for the name of the folder that contains the module unit-tests.
+        'provider_path' => 'Providers', // This value is for the name of the folder that contains the module providers.
     ],
 
     /*
      * Queries.
+     *
+     * This is some config for add queries.
      */
     'queries' => [
+        /*
+         * Except columns in fillable.
+         *
+         * This `except_columns_in_fillable` must be arrayed!
+         * This `except_columns_in_fillable` that remove field from $fillable in model.
+         */
         'except_columns_in_fillable' => [
             'id', 'updated_at', 'created_at'
         ]
     ],
 
     /*
-     * Get main controlelr.
+     * Get main controller.
+     *
+     * This is a namespace of main controller that default path is `App\Http\Controllers\Controller`.
      */
     'main_controller' => 'App\Http\Controllers\Controller'
 ];
