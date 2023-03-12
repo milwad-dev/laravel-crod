801f4bb538a1e3928c6aa1ba94e65cae2baf9a5a
miladev-ent <milwad.dev@gmail.com>
2023-03-12 10:27:49 +0330

    add CommonTrait

--- src/Commands/MakeCrudCommand.php
+++ src/Commands/MakeCrudCommand.php
@@ -186,34 +186,4 @@ class MakeCrudCommand extends Command
             'name' => $name . 'Factory'
         ]);
     }
-
-    /**
-     * Show extra option in CLI.
-     *
-     * @return array|string
-     */
-    private function extraOption(): string|array
-    {
-        return $this->choice('You want something extra?', OptionData::$options, 0);
-    }
-
-    /**
-     * Extra option operation.
-     *
-     * @param  string $name_uc
-     * @return void
-     */
-    public function extraOptionOperation(string $name_uc)
-    {
-        $selectOption = $this->extraOption();
-
-        if ($selectOption === OptionData::SEEDER_OPTION) {
-            $this->makeSeeder($name_uc);
-            $this->extraOptionOperation($name_uc);
-        }
-        if ($selectOption === OptionData::FACTORY_OPTION) {
-            $this->makeFactory($name_uc);
-            $this->extraOptionOperation($name_uc);
-        }
-    }
 }
